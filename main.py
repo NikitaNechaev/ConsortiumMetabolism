3008b7a569a09c1304401a5956dc2fe00455d0bc
NikitaNechaev <2805niki05ta@gmail.com>
2024-09-15 00:46:28 +0300

    add file output

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ import numpy as np
 import xml.etree.ElementTree as et
 import sys
 import os
-
+from tqdm import tqdm
 
 np.set_printoptions(threshold=sys.maxsize)
-
-tree = et.parse('XML_Data/eco00220.xml')
-root = tree.getroot()
-
-#for i in root.findall('entry'):
-#    print(i.attrib["id"], '|', str(i.attrib["name"].split(" ")[0]))
     
 class MapMatrix:
     matrix_headers = []
@@ -20,9 +14,9 @@ class MapMatrix:
     def __init__(self, file:et) -> None:
         self.map_file = file
         self.map_root = file.getroot()
-        self.map_name = root.attrib["name"]
-        self.map_org = root.attrib["org"]
-        self.map_number = root.attrib["number"]
+        self.map_name = self.map_root.attrib["name"]
+        self.map_org = self.map_root.attrib["org"]
+        self.map_number = self.map_root.attrib["number"]
         
         self.substances_dict = self.headers_init()
         
@@ -37,7 +31,7 @@ class MapMatrix:
         
     def headers_init(self) -> dict:
         headers = {}
-        for i in root.findall('entry'):
+        for i in self.map_root.findall('entry'):
             headers[i.attrib["id"]] = str(i.attrib["name"].split(" ")[0])
         return headers
     
@@ -45,7 +39,7 @@ class MapMatrix:
         reacts = {}
         self.sub_m = ''
         self.prd_m = ''
-        for i in root.findall('reaction'):
+        for i in self.map_root.findall('reaction'):
             self.sub_m = self.matrix_headers.index(str(i.find("substrate").attrib["id"]))
             self.prd_m = self.matrix_headers.index(str(i.find("product").attrib["id"]))
             self.matrix[self.sub_m, self.prd_m] = i.attrib["id"]
@@ -77,23 +71,23 @@ class MapMatrix:
     def crosshair(self):
         for i in range(len(self.matrix[:,0])):
             for l in range(len(self.matrix[0,:])):
-                if np.sum(self.matrix[:,0]) == np.sum(self.matrix[0,:]):
-                    self.enders.append(self.substances_dict[self.matrix_headers[self.matrix[i][l]]])
+                if np.sum(self.matrix[:,0]) == np.sum(self.matrix[0,:]) and np.sum(self.matrix[:,0]) != 0:
+                    self.enders.append(self.substances_dict[self.matrix_headers.index(int(self.matrix[i][l]))])
 
 
 maps = []
-for i in os.listdir("XML_Data"):
+for i in tqdm(os.listdir("XML_Data")):
     tree = et.parse(f'XML_Data/{i}')
     maps.append(MapMatrix(tree))
     
 full_dict = {}
 
+f = open('out.txt', 'w')
+
 for i in maps:
     full_dict[i.map_name] = {"starters": i.starters, "enders": i.enders}
-for i in maps:
+for i in tqdm(maps):
     for l in maps:
         for j in range(len(full_dict[i.map_name]["starters"])):
             if full_dict[i.map_name]["starters"][j] in full_dict[l.map_name]["enders"]:
-                print(f"{full_dict[i.map_name]}/{full_dict[i.map_name]["starters"][j]} in {full_dict[l.map_name]}")
-    
-#print(full_dict)
+                f.write(f"{i.map_name}, {l.map_name}, {full_dict[i.map_name]["starters"][j]}\n")
